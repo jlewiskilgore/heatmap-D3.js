6f0df180ca4c606ea449791664f4838726fb19a9
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-04-13 18:12:11 -0400

    Add color rects to grid in axis

--- js/HeatMap.js
+++ js/HeatMap.js
@@ -25,50 +25,67 @@ d3.json(dataUrl, function(json) {
 	console.log(maxTempVariance);
 
 	var height = 600;
-	var width = 600;
+	var width = 1400;
 
-	var colors = ["blue", "lightblue", "green", "lightgreen", "yellow", "lightorange", "orange", "lightred", "red"];
+	var colors = ["blue", "lightblue", "green", "lightgreen", "yellow", "gold", "orange", "lightred", "red"];
 
 	d3.select(".heatmap-title").text("Heat Map of Global Temperatures");
 
 	var svg = d3.select(".heatmap")
 		.attr("width", width)
 		.attr("height", height)
-		.attr("viewBox", "0 0 550 550");
+		.attr("viewBox", "200 0 750 550");
 
 	var colorScale = d3.scaleQuantize()
 		.domain([baseTemperature + minTempVariance, baseTemperature + maxTempVariance])
 		.range(colors);
 
 	for(var i = 0; i < varianceData.length; i++) {
-		console.log(yearData[i] + " " + colorScale(varianceData[i]+baseTemperature));
+		console.log(yearData[i] + " " + colorScale(varianceData[i] + baseTemperature));
 	}
+
+	var rects = svg.selectAll("rect")
+		.data(dataSetMonthlyVariance)
+		.enter()
+		.append("rect")
+		.attr("x", function(d, i) {
+			return ((d.year) - 1675 + (i/4));
+		})
+		.attr("y", function(d, i) {
+			return ((d.month) * 40);
+		})
+		.attr("width", 5)
+		.attr("height", 20)
+		.style("fill", function(d) {
+			return colorScale(d.variance + baseTemperature);
+		});
+
 
 	// X Axis
 	var xScale = d3.scaleLinear()
 		.domain([1750, 2015])
-		.range([0, 400]);
+		.range([0, 1055]);
 
 	var xAxis = d3.axisBottom()
 		.scale(xScale)
-		.ticks(10);
+		.ticks(15);
 
 	svg.append("g")
-		.attr("transform", "translate(75," + 450 + ")")
+		.attr("transform", "translate(75," + 500 + ")")
 		.call(xAxis);
 
 
 	// Y Axis
 	var yScale = d3.scaleLinear()
 		.domain([1, 12])
-		.range([0, 400]);
+		.range([0, 475]);
 
 	var yAxis = d3.axisLeft()
 		.scale(yScale)
 		.ticks(10);
 
 	svg.append("g")
-		.attr("transform", "translate(75," + 50 + ")")
+		.attr("transform", "translate(75," + 25 + ")")
 		.call(yAxis);
 
 });
